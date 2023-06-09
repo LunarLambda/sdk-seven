2117d99e38df5a60c0c457ed4f12a9a1906cb407
Luna Saphie Mittelbach <lunarlambda@gmail.com>
2023-06-09 15:17:51 +0200

    tested inline_swi with lto

--- libseven/doc/rnd/inline_swi.h
+++ libseven/doc/rnd/inline_swi.h
@@ -3,9 +3,9 @@
 // swi N << ((1f - . == 4) * -16); 1:
 //
 // clobbers: r0, r1, r3, memory
-// to test: do register variables behave w/ LTO?
+// make sure to always set input operands as +r outputs because of clobbering
 //
 // use inline rather than static inline so fallback to asm'd definitions
 // possible (for FFI and debug builds)
 //
-// __attribute__((target(...), noinline, section(...)) works with LTO.
+// __attribute__((target(...), noinline, section(...)) works with LTO too.
